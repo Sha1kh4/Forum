4107e4fb615b1d9e4c9c35752435fd70035bdff3
Shaikh Adnan <117739824+Sha1kh4@users.noreply.github.com>
2025-12-16 11:41:55 +0000

    Refactor main.py to include database session management and update health check endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,43 @@
-from typing import Union
+from typing import Union , Annotated
+from fastapi import FastAPI, Depends ,HTTPException
+import auth
+import models
+from db import engine, SessionLocal 
+from auth import get_current_user
 
-from fastapi import FastAPI
+
 
 app = FastAPI()
 
+app.include_router(auth.router)
+ 
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():  
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[SessionLocal, Depends(get_db)]
+user_dependency = Annotated[models.Users, Depends(get_current_user)]
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    return {"Health Check": "OK"}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/question")
+async def create_question(  db: db_dependency, question: str):
+    new_question = models.Questions(userid=67, message=question)
+    db.add(new_question)
+    db.commit()
+    db.refresh(new_question)
+    return new_question
+
+@app.get("/questions")
+async def get_questions(db: db_dependency):
+    questions = db.query(models.Questions).all()
+    return questions
