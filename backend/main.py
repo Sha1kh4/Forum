cfbc49f26893902010b53ba19c4216316d469f1e
Shaikh Adnan <117739824+Sha1kh4@users.noreply.github.com>
2025-12-16 16:23:31 +0000

    add CORS

--- backend/main.py
+++ backend/main.py
@@ -4,10 +4,19 @@ import auth
 import models
 from db import engine, SessionLocal 
 from auth import get_current_user
-
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
  
@@ -41,3 +50,17 @@ async def create_question(  db: db_dependency, question: str):
 async def get_questions(db: db_dependency):
     questions = db.query(models.Questions).all()
     return questions
+
+@app.post("/answer")
+async def create_answer( db: db_dependency, questionid: int, answer: str):
+    new_answer = models.Answers(questionid=questionid, userid=67, message=answer)
+    db.add(new_answer)
+    db.commit()
+    db.refresh(new_answer)
+    return new_answer
+
+
+@app.get("/answers/{questionid}")
+async def get_answers(questionid: int, db: db_dependency):
+    answers = db.query(models.Answers).filter(models.Answers.questionid == questionid).all()
+    return answers
